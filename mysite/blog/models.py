a86acb3599674379e19895f6ee6287d70db2dcd7
Muhammad Ali <muhammadali@baarei.com>
2017-09-08 19:47:19 +0500

    added comenting feature

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -22,3 +22,20 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments')
+    author = models.ForeignKey('auth.User')
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    is_approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.is_approved = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ['-created_date']
--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/edit/(?P<pk>\d+)/$', views.post_edit, name='post_edit'),
-    url(r'^post/publish/(?P<pk>\d+)/$', views.post_publish, name='post_publish'),
-    url(r'^post/unpublish/(?P<pk>\d+)/$', views.post_unpublish, name='post_unpublish'),
-    url(r'^post/delete/(?P<pk>\d+)/$', views.post_delete, name='post_delete'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
+    url(r'^post/(?P<pk>\d+)/unpublish/$', views.post_unpublish, name='post_unpublish'),
+    url(r'^post/(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
+    url(r'^post/(?P<pk>\d+)/comment/new$', views.add_comment_to_post, name='add_comment_to_post'),
+    url(r'^comment/(?P<pk>\d+)/delete/$', views.comment_delete, name='comment_delete'),
 ]
